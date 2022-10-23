99ffa30dc9b898b3bb093482df5cd8901082de7a
Ivan Aispuro <iaispuro@HallenConstruction.onmicrosoft.com>
2022-10-23 15:57:25 -0600

    custom validations

--- WebApiAutores/WebApiAutores/Entities/Libro.cs
+++ WebApiAutores/WebApiAutores/Entities/Libro.cs
@@ -1,8 +1,11 @@
-﻿namespace WebApiAutores.Entities
+﻿using WebApiAutores.Validators;
+
+namespace WebApiAutores.Entities
 {
     public class Libro
     {
         public int Id { get; set; }
+        [PrimeraLetraMayuscula]
         public string? Titulo { get; set; }
         public int AutorId { get; set; }
         public Autor? Autor { get; set; }
